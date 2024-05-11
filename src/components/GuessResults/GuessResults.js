79ffabd9cd60fe0417d2024d94a86e8445b25a55
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-11 15:28:29 -0500

    Import Utilities & Map Over Guess Component

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Guess from "../Guess";
+import NUM_OF_GUESSES_ALLOWED from "../../constants";
+import range from "../../utils";
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, i) => (
-        <p className="guess" key={`${guess}-${Math.random()}`}>
-          {guess}
-        </p>
+      {guesses.map((guess) => (
+        <Guess value={guess} key={`${guess}-${Math.random()}`} />
       ))}
     </div>
   );
