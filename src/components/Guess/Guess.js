cc9f0aa4e215d0cf96b2420ec44286742554220d
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-11 17:37:28 -0500

    Import checkGuess Helper Function & Dynamically Render Tile Color Based on Correctness

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,15 +1,25 @@
 import React from "react";
 //helper function to generate an array of a given length
 import { range } from "../../utils";
+//another helper to determine a letter's tile color
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
   return (
     <>
       <p className="guess">
         {/* Generates the guess row */}
         {range(5).map((num, i) => (
           // generates a span to hold the letter, or an empty string if no value exists
-          <span key={`${num}-${Math.random()}`} className="cell">
+          <span
+            key={`${num}-${Math.random()}`}
+            className={
+              //generates a color on the correctness of a letter
+              checkGuess(value, answer)
+                ? `cell ${checkGuess(value, answer)[i].status}`
+                : "cell"
+            }
+          >
             {value ? value[num] : ""}
           </span>
         ))}
