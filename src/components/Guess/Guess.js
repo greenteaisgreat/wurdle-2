2c172f9083e7cdeb9322e74be67a11d7fcab4f42
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-11 16:09:45 -0500

    Use range() to Generate 5 Rows to Hold Guesses

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { range } from "../../utils";
 
 function Guess({ value }) {
   return (
     <>
       <p className="guess">
-        {value.split("").map((letter, i) => (
-          <span key={`${letter}-${Math.random()}`} className="cell">
-            {letter}
+        {range(5).map((num, i) => (
+          <span key={`${num}-${Math.random()}`} className="cell">
+            {value ? value[num] : ""}
           </span>
         ))}
       </p>
