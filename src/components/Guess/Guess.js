293c2a649a459932c8434754d2ea30cc98b6f4c2
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-11 16:29:26 -0500

    Add Helpful Comments for Future Self/Others😘

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,14 @@
 import React from "react";
+//helper function to generate an array of a given length
 import { range } from "../../utils";
 
 function Guess({ value }) {
   return (
     <>
       <p className="guess">
+        {/* Generates the guess row */}
         {range(5).map((num, i) => (
+          // generates a span to hold the letter, or an empty string if no value exists
           <span key={`${num}-${Math.random()}`} className="cell">
             {value ? value[num] : ""}
           </span>
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Guess from "../Guess";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+//helper function to generate arrays of a specified length
 import { range } from "../../utils";
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
+      {/* Generates the game's grid */}
       {range(6).map((num) => (
         <Guess
+          //Checks for a value; if none, an empty string populates the value
           value={guesses ? guesses[num] : ""}
           key={`${num}-${Math.random()}`}
         />
