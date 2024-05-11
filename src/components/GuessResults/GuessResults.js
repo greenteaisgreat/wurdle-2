ba005ee7eeeb8bea886e69cf1085e246ce390df7
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-11 16:10:41 -0500

    Use range() to Generate 6 Guess Components

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Guess from "../Guess";
-import NUM_OF_GUESSES_ALLOWED from "../../constants";
-import range from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess) => (
-        <Guess value={guess} key={`${guess}-${Math.random()}`} />
+      {range(6).map((num) => (
+        <Guess
+          value={guesses ? guesses[num] : ""}
+          key={`${num}-${Math.random()}`}
+        />
       ))}
     </div>
   );
