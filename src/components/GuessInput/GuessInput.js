c7a67bd4aa8a2a93294d4edbdbd07b029596c9dd
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-12 16:54:29 -0500

    Add Ternary Solution for GuessInput End-Game Logic

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,8 +2,9 @@ import React from "react";
 import EndGameBanner from "../EndGameBanner";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
-function GuessInput({ handleSubmitGuess }) {
+function GuessInput({ handleSubmitGuess, gameStatus, guesses, answer }) {
   const [tentativeGuess, setTentativeGuess] = React.useState("");
+  const turns = guesses.length;
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -16,19 +17,27 @@ function GuessInput({ handleSubmitGuess }) {
     <>
       <form onSubmit={handleSubmit} className="guess-input-wrapper">
         <label htmlFor="guess-input">Enter Guess:</label>
-        <input
-          required
-          id="guess-input"
-          type="text"
-          placeholder="Listen to Your Instincts..."
-          value={tentativeGuess}
-          pattern="[a-z, A-Z]{5,5}"
-          maxLength={5}
-          title="Enter 5 Letters (No Numbers/Symbols)"
-          onChange={(e) => {
-            setTentativeGuess(e.target.value.toUpperCase());
-          }}
-        ></input>
+        {gameStatus === "running" ? (
+          <input
+            required
+            id="guess-input"
+            type="text"
+            placeholder="Listen to Your Instincts..."
+            value={tentativeGuess}
+            pattern="[a-z, A-Z]{5,5}"
+            maxLength={5}
+            title="Enter 5 Letters (No Numbers/Symbols)"
+            onChange={(e) => {
+              setTentativeGuess(e.target.value.toUpperCase());
+            }}
+          ></input>
+        ) : (
+          <EndGameBanner
+            turns={turns}
+            gameStatus={gameStatus}
+            answer={answer}
+          />
+        )}
       </form>
     </>
   );
