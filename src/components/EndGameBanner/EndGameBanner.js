840f4503b87ed3f56bd469da6d32cb7727bc7cf5
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-11 21:27:29 -0500

    Add Ending Game Banner Component

---
+++ src/components/EndGameBanner/EndGameBanner.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+
+function EndGameBanner({ hasWon, answer, turns }) {
+  return (
+    <>
+      {hasWon ? (
+        <div className="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it
+            {turns === NUM_OF_GUESSES_ALLOWED ? (
+              <strong style={{ color: "goldenrod" }}>
+                {" "}
+                ON THE LAST GUESS!
+              </strong>
+            ) : (
+              <strong style={{ color: "goldenrod" }}> {turns} guesses.</strong>
+            )}
+          </p>
+        </div>
+      ) : (
+        <div className="sad banner">
+          <p>
+            Sorry, the correct answer is{" "}
+            <strong style={{ color: "gold" }}>{answer}</strong>.
+          </p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default EndGameBanner;
