b7103b7e727c97decd302f59a1c421f580436702
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-07-21 16:39:14 -0500

    Add Vercel analytics component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import Game from "../Game";
 import Header from "../Header";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 
       <div className="game-wrapper">
         <Game />
+        <Analytics />
       </div>
     </div>
   );
