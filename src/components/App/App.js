ffd961af04993427f26b53783487acc0b334fe13
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-07-21 16:47:17 -0500

    Remove poopy Vercel analytics component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import Game from "../Game";
 import Header from "../Header";
-import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
 
       <div className="game-wrapper">
         <Game />
-        <Analytics />
       </div>
     </div>
   );
